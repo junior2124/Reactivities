6b4a556413a88cd2e4b7a19fa3af0d958e04c04b
escobede <escobede@LESCOBEDOET450>
2019-12-09 14:56:59 -0600

    End of API section.

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,7 +11,7 @@ namespace Persistence
     }
 
     public DbSet<Value> MyProperty { get; set; }
-
+    public DbSet<Activity> Activities { get; set; }
     protected override void OnModelCreating(ModelBuilder builder)
     {
       builder.Entity<Value>()
